45624dcb37b89337e8a9f2919d62edf3e8a6e96c
logic-zero <vistalzero@gmail.com>
2025-04-02 07:34:19 +0800

    t

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,7 +33,7 @@ class HandleInertiaRequests extends Middleware
             ...parent::share($request),
             'auth' => [
                 'user' => $request->user(),
-                'roles' => $request->user()->roles->pluck('name')
+                // 'roles' => $request->user()->roles->pluck('name')
             ],
 
             'flash' => [
