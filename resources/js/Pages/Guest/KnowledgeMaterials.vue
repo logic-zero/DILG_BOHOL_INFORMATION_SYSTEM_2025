96cbad0b27205d2868cb294eba4eefc7a40f8a7d
logic-zero <vistalzero@gmail.com>
2025-04-02 15:28:22 +0800

    flipbook loading animation

--- resources/js/Pages/Guest/KnowledgeMaterials.vue
+++ resources/js/Pages/Guest/KnowledgeMaterials.vue
@@ -16,12 +16,18 @@ const filters = ref({
 
 const showModal = ref(false);
 const currentFlipbookUrl = ref("");
+const isLoading = ref(false);
 
 const openFlipbook = (pdfUrl) => {
     const encodedPdfUrl = encodeURIComponent(pdfUrl);
     const randomParam = Math.random().toString(36).substring(7);
     currentFlipbookUrl.value = `https://heyzine.com/api1?pdf=${encodedPdfUrl}&k=de98c2e60096a623&r=${randomParam}`;
     showModal.value = true;
+    isLoading.value = true;
+};
+
+const handleIframeLoad = () => {
+    isLoading.value = false;
 };
 
 const openInFullPage = () => {
@@ -121,7 +127,7 @@ const goToPage = (url) => {
             </div>
         </div>
 
-        <div v-if="showModal" class="fixed inset-0 z-50 bg-white flex items-center justify-center p-0">
+        <div v-if="showModal" class="fixed inset-0 z-50 bg-black bg-opacity-5 flex items-center justify-center p-0">
             <div class="absolute inset-0 flex flex-col">
                 <div class="bg-white p-2 flex justify-end">
                     <div class="space-x-2">
@@ -136,11 +142,146 @@ const goToPage = (url) => {
                     </div>
                 </div>
 
-                <iframe :src="currentFlipbookUrl"
-                        class="flex-1 border-0 w-full h-full"
-                        allowfullscreen>
+                <div v-if="isLoading" class="flex-1 flex items-center justify-center">
+                    <div class="text-center">
+                        <div class="book">
+                            <div class="inner">
+                                <div class="left"></div>
+                                <div class="middle"></div>
+                                <div class="right"></div>
+                            </div>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <p class="mt-4 text-gray-800">Preparing flipbook...</p>
+                    </div>
+                </div>
+
+                <iframe
+                    :src="currentFlipbookUrl"
+                    @load="handleIframeLoad"
+                    class="flex-1 border-0 w-full h-full"
+                    :class="{ 'hidden': isLoading }"
+                    allowfullscreen>
                 </iframe>
             </div>
         </div>
     </div>
 </template>
+
+<style scoped>
+.book {
+    width: 60px;
+    height: 80px;
+    position: relative;
+    perspective: 150px;
+    margin: 0 auto;
+}
+
+.book .inner {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+    animation: flip 2s infinite ease-in-out;
+}
+
+.book .left, .book .middle, .book .right {
+    position: absolute;
+    width: 33.33%;
+    height: 100%;
+    background: #3b82f6;
+    transform-origin: right center;
+}
+
+.book .left {
+    left: 0;
+    animation: leftPage 2s infinite ease-in-out;
+}
+
+.book .middle {
+    left: 33.33%;
+    background: #1d4ed8;
+}
+
+.book .right {
+    right: 0;
+    transform-origin: left center;
+    animation: rightPage 2s infinite ease-in-out;
+}
+
+.book ul {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    transform-style: preserve-3d;
+}
+
+.book ul li {
+    position: absolute;
+    list-style: none;
+    background: #fff;
+    border: 1px solid #ddd;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    animation: pages 2s infinite ease-in-out;
+}
+
+.book ul li:nth-child(1) { animation-delay: 0.05s; }
+.book ul li:nth-child(2) { animation-delay: 0.1s; }
+.book ul li:nth-child(3) { animation-delay: 0.15s; }
+.book ul li:nth-child(4) { animation-delay: 0.2s; }
+.book ul li:nth-child(5) { animation-delay: 0.25s; }
+.book ul li:nth-child(6) { animation-delay: 0.3s; }
+.book ul li:nth-child(7) { animation-delay: 0.35s; }
+.book ul li:nth-child(8) { animation-delay: 0.4s; }
+.book ul li:nth-child(9) { animation-delay: 0.45s; }
+.book ul li:nth-child(10) { animation-delay: 0.5s; }
+.book ul li:nth-child(11) { animation-delay: 0.55s; }
+.book ul li:nth-child(12) { animation-delay: 0.6s; }
+.book ul li:nth-child(13) { animation-delay: 0.65s; }
+.book ul li:nth-child(14) { animation-delay: 0.7s; }
+.book ul li:nth-child(15) { animation-delay: 0.75s; }
+.book ul li:nth-child(16) { animation-delay: 0.8s; }
+.book ul li:nth-child(17) { animation-delay: 0.85s; }
+
+@keyframes flip {
+    0%, 100% { transform: rotateY(-20deg); }
+    50% { transform: rotateY(20deg); }
+}
+
+@keyframes leftPage {
+    0%, 100% { transform: rotateY(0); }
+    50% { transform: rotateY(-15deg); }
+}
+
+@keyframes rightPage {
+    0%, 100% { transform: rotateY(0); }
+    50% { transform: rotateY(15deg); }
+}
+
+@keyframes pages {
+    0%, 100% { opacity: 0; transform: translateZ(5px); }
+    50% { opacity: 0.5; transform: translateZ(0); }
+}
+</style>
